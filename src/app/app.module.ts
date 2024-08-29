325f65b58147eb35ed6c7ca14d138ea694870611
Elizabeth.Primeau <elizabeth.primeau@tc.gc.ca>
2024-08-29 15:10:36 -0400

    added home,maps,navbar skeleton files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes, RouterModule, provideRouter } from '@angular/router';
+import routeConfig from './routes';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MapsComponent } from './maps/maps.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MapsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routeConfig)
   ],
-  providers: [],
+  providers: [
+    provideRouter(routeConfig)
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ src/app/maps/maps.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+
+@Component({
+  selector: 'app-maps',
+  templateUrl: './maps.component.html',
+  styleUrls: ['./maps.component.scss']
+})
+export class MapsComponent {
+
+}
