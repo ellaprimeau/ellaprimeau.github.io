513f952eb7d832057fa7b16e3c0c30b0e8546a8e
Elizabeth.Primeau <elizabeth.primeau@tc.gc.ca>
2024-09-05 08:42:02 -0400

    added pages under maps

---
+++ src/app/maps/map-bar/map-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-bar',
+  templateUrl: './map-bar.component.html',
+  styleUrls: ['./map-bar.component.scss']
+})
+export class MapBarComponent {
+
+}
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { MapsComponent } from './maps/maps.component';
+import { RouteInfoComponent } from './maps/route-info/route-info.component';
 import { HomeComponent } from './home/home.component';
 
 const routeConfig: Routes = [
   {path: 'maps', component: MapsComponent},
+  {path: 'maps/routeInfo', component: RouteInfoComponent},
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
