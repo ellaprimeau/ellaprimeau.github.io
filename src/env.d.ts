61bda8d3e446bf7fc452d51a90e83858876ea415
Elizabeth Primeau <bprim080@uottawa.ca>
2024-09-22 15:35:38 -0400

    stilll

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,4 +5,7 @@ interface ImportMeta {
 interface ImportMetaEnv {
   readonly NODE_ENV: string;
   readonly NG_APP_NAME: string;
+  readonly NG_APP_CONTENTFUL_ACCESS_TOKEN: string;
+  readonly NG_APP_CONTENTFUL_PREVIEW_ACCESS_TOKEN: string;
+  readonly NG_APP_CONTENTFUL_SPACE_ID: string;
 }
