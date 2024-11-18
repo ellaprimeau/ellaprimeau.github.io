9b61fa440097536806e480469e75da3a4f336791
Elizabeth.Primeau <ella.j.primeau@gmail.com>
2024-11-18 14:51:50 -0500

    reverting to osm

--- src/app/maps/route-info/route-info.component.ts
+++ src/app/maps/route-info/route-info.component.ts
@@ -14,7 +14,7 @@ export class RouteInfoComponent implements AfterViewInit {
       center: [39.8282, -98.7594],
       zoom: 3
     });
-    const tiles = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}.png', {
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
       minZoom: 3,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -22,6 +22,7 @@ export class RouteInfoComponent implements AfterViewInit {
 
     tiles.addTo(this.map);
 
+    
   }
   constructor() {}
 
